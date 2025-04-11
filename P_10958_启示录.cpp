f48874d27109ca03f9c550aded6a1eb52474137f
Worldcpu <153879880@qq.com>
2025-04-11 08:30:38 +0800

    test

---
+++ P_10958_启示录.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+constexpr int MN=20;
+
+ll f[MN][11][11];
+
+ll dfs(int pos,int lt1,int lt2,int lim){
+
+}
+
+void solve(ll x){
+    
+}
+
+
+int main(){
+
+    return 0;
+}
